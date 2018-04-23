6f55d6304eb85e6bf02159b7f06aba39ee0b658b
Kevin Luo <kevin.luo@linecorp.com>
2018-04-23 21:01:13 +0800

    change heroku deploy files

--- app.py
+++ app.py
@@ -52,6 +52,7 @@ def callback():
     # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
+    print('Event: ' + body)
 
     # parse webhook body
     try:
